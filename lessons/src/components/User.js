87c435ddcbce1be410f1f117eec9b61f40b0bac0
argun8394 <argundoganali@gmail.com>
2021-09-08 15:19:09 +0300

    key prop

--- lessons/src/components/User.js
+++ lessons/src/components/User.js
@@ -1,7 +1,14 @@
 // Koşullu render işlemi
 
-function User({name, surname, isLoggedIn,age/*props*/}){
-    return <h1> {isLoggedIn ? `My name is ${name} and my surname is ${surname}, ${age}` :"Dont Entry"}</h1>
-};
+function User({name, surname, isLoggedIn,age,friends/*props*/}){
+    return(
+    <>
+     <h1> {isLoggedIn ? `My name is ${name} and my surname is ${surname}, ${age}` :"Dont Entry"}</h1>
+     {friends.map((friend,index) => 
+     (<div key={index}>{index}-{friend}</div>))
+     }
+     </>);
+}
+
 
 export default User;
