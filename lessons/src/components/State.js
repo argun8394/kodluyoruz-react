c534e1591a892f5aef9fcea36bf5d646d4993e19
argun8394 <argundoganali@gmail.com>
2021-09-09 21:33:34 +0300

    state

--- lessons/src/components/State.js
+++ lessons/src/components/State.js
@@ -9,6 +9,8 @@ function State() {
             <h1>Merhaba {name}</h1>
             <h2>{age}</h2>
 
+            <button onClick={()=>setName("Tahir")}>Change Name</button>
+            <button onClick={()=>setAge("15")}>Change Age</button>
         </div>
     );
     
